1f39ffcfa4b3b88106cb3fedd5210e874fd418a1
No Name <KingOfLife2468@hotmail.com>
2014-02-14 18:02:50 +0000

    Update AutoWelcome.js

--- AutoWelcome.js
+++ AutoWelcome.js
@@ -7,7 +7,7 @@ API.on(API.USER_JOIN, function(data){
 if(message == ""){
 API.sendChat(" @" + data.username + ":Welcome to 'i'm not BR ");
 } else {
-API.sendChat(message + " @" + data.username);
+API.sendChat(" @" + data.username + message);
 
 }
 });
