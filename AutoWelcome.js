93e1b1682452a71610c77050de6406e1095cfd83
No Name <KingOfLife2468@hotmail.com>
2014-02-14 18:11:31 +0000

    Update AutoWelcome.js

--- AutoWelcome.js
+++ AutoWelcome.js
@@ -4,8 +4,8 @@ message = prompt("Message here the username will go at the begin");
 
 API.on(API.USER_JOIN, function(data){
 
-if(message == ""){
-API.sendChat(" @" + data.username + " :Welcome to 'i'm not BR ");
+if(message == "Welcome to ♕ Portugal Eletro Music 【PEM】 ♕"){
+API.sendChat(" @" + data.username + " Welcome to ♕ Portugal Eletro Music 【PEM】 ♕ ");
 } else {
 API.sendChat(" @" + data.username + message);
 
