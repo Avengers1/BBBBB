d9952dc0704a79bf210f301e13a08020931f1605
No Name <KingOfLife2468@hotmail.com>
2014-02-14 18:06:24 +0000

    Update AutoWelcome.js

--- AutoWelcome.js
+++ AutoWelcome.js
@@ -1,11 +1,11 @@
 //AUTO WELCOME!!!
 
-message = prompt("Message here the username will go at the end");
+message = prompt("Message here the username will go at the begin");
 
 API.on(API.USER_JOIN, function(data){
 
 if(message == ""){
-API.sendChat(" @" + data.username + ":Welcome to 'i'm not BR ");
+API.sendChat(" @" + data.username + " :Welcome to 'i'm not BR ");
 } else {
 API.sendChat(" @" + data.username + message);
 
